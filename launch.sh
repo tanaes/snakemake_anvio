0659b87efeac657d0652066b35d8bf2a9d976938
Tanaes <jonsan@gmail.com>
2016-07-29 10:58:42 -0700

    Removed queue specification from qsub call

--- launch.sh
+++ launch.sh
@@ -5,6 +5,6 @@ if [ "$#" -lt 1 ]; then
     exit
 fi
 
-snakemake -j 100 --local-cores 8 --cluster-config cluster.json --cluster "qsub -q {cluster.queue} -k eo -m n -l nodes=1:ppn={cluster.n} -l mem={cluster.mem}gb -l walltime={cluster.time}" --directory "$@"
+snakemake -j 100 --local-cores 8 --cluster-config cluster.json --cluster "qsub -k eo -m n -l nodes=1:ppn={cluster.n} -l mem={cluster.mem}gb -l walltime={cluster.time}" --directory "$@"
 #snakemake --directory ~/pipeline_test/exp1/ --dag | dot -Tsvg > dag.svg 
 #-o /home/snurk/logs/ -e /home/snurk/logs/ 
